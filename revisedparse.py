3ba0101a4809918e666ece3a6f375ca6dd6d87de
tysalim <tylersamuellim@gmail.com>
2025-11-01 22:20:16 +0800

    revised

--- revisedparse.py
+++ revisedparse.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 # === CONFIG ===
 input_file = "/Users/tysali/Downloads/AI_Human.csv"
-output_file = "subset_lg.csv"
-n_samples_per_class = 10000
+output_file = "subset_med.csv"
+n_samples_per_class = 5000
 chunksize = 50000
 
 collected = {True: [], False: []}
